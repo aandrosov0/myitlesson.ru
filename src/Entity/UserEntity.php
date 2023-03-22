1cabddfd8933a8883044e85edde12224576e6fe6
aandrosov <aandrosov338@gmail.com>
2023-03-22 20:43:01 +0300

    added courses and authoredCourses properties

--- src/Entity/UserEntity.php
+++ src/Entity/UserEntity.php
@@ -1,16 +1,25 @@
 <?php 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'user')]
 class UserEntity extends Entity {
 	#[ORM\Column(type: 'string', length: 16)]
-	protected string $username;
+	private string $username;
 
 	#[ORM\Column(type: 'string', length: 32)]
-	protected string $password;
+	private string $password;
+
+	#[ORM\JoinTable(name: 'user_courses'), ORM\JoinColumn(name: 'user_id'), ORM\InverseJoinColumn(name: 'course_id')]
+	#[ORM\ManyToMany(targetEntity: CourseEntity::class, inversedBy: 'users', cascade: ['persist'])]
+	private Collection $courses;
+
+	#[ORM\OneToMany(targetEntity: CourseEntity::class, mappedBy: 'author', cascade: ['persist'])]
+	private Collection $authoredCourses;
 
 	public static function new(string $username, string $password) {
 		return (new UserEntity())
@@ -19,7 +28,8 @@ class UserEntity extends Entity {
 	}
 
 	public function __construct() {
-		
+		$this->courses = new ArrayCollection();
+		$this->authoredCourses = new ArrayCollection();
 	}
 
     public function getUsername() {
@@ -39,4 +49,47 @@ class UserEntity extends Entity {
         $this->password = $password;
         return $this;
     }
+
+    public function getCourses() {
+        return $this->courses;
+    }
+
+    public function setCourses(Collection $courses) {
+        $this->courses = $courses;
+        return $this;
+    }
+
+    public function getAuthoredCourses() {
+        return $this->authoredCourses;
+    }
+
+    public function setAuthoredCourses(Collection $authoredCourses) {
+        $this->authoredCourses = $authoredCourses;
+        return $this;
+    }
+
+	public function addCourse(CourseEntity $course) {
+		$this->courses->add($course);
+		$course->getUsers()->add($this);
+
+		return $this;
+	}
+
+	public function removeCourse(CourseEntity $course) {
+		$this->courses->removeElement($course);
+		$course->getUsers()->removeElement($this);
+
+		return $this;
+	}
+
+	public function addAuthoredCourse(CourseEntity $course) {
+		$course->setAuthor($this);
+		return $this;
+	}
+
+	public function removeAuthoredCourse(CourseEntity $course) {
+		$course->removeAuthor();
+
+		return $this;
+	}
 }
