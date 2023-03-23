29e5b9892082e2e7d243c094d519d21363556685
aandrosov <aandrosov338@gmail.com>
2023-03-23 21:05:11 +0300

    added function add

--- src/View/UserView.php
+++ src/View/UserView.php
@@ -14,4 +14,29 @@ class UserView {
 
 		return new JSONMessage($user->toArray());
 	}
+
+	public static function add($username, $password) {
+		$user = UserEntity::getRepository()->findOneBy(['username' => $username]);
+
+		if(isset($user)) {
+			return new JSONMessage(['err' => "User with username '$username' is already exists!", 'status_code' => JSONMessage::ALREADY_EXISTS], 404);
+		} 
+
+		$user = UserEntity::new($username, $password);
+		UserEntity::add($user);
+
+		return new JSONMessage(['id' => $user->getId()]);
+	}
+
+	public static function remove($username) {
+		$user = UserEntity::getRepository()->findOneBy(['username' => $username]);
+
+		if(!isset($user)) {
+			return new JSONMessage(['err' => "User with username '$username' ins't exists!", 'status_code' => JSONMessage::NOT_FOUND], 404);
+		}
+
+		UserEntity::delete($user);
+
+		return new JSONMessage([]);
+	}
 }
