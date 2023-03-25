1b30d4f8fb5993546c18061cadbfbd60413c1d90
aandrosov <aandrosov338@gmail.com>
2023-03-25 19:27:05 +0300

    added constants

--- src/Rendering/Message.php
+++ src/Rendering/Message.php
@@ -5,6 +5,8 @@ abstract class Message {
 	public const NOT_FOUND = 0;
 	public const ALREADY_EXISTS = 1;
 	public const QUERY_EMPTY = 2;
+	public const PAGE_NOT_FOUND = 3;
+	public const AUTH_ERROR = 4;
 
     public function __construct(protected string $message, protected int $responseCode, protected string $headerContent) {}
     
