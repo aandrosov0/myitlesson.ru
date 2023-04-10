174f751edff8cd7c49e001b2a5676e54f6f93d73
aandrosov <aandrosov338@gmail.com>
2023-04-10 16:13:56 +0300

    renamde links in navigation panel

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,3 @@
-<?php global $user; ?>
-
 <!DOCTYPE html>
 <html lang="en" style="height: 100%;">
 
@@ -8,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="/public/css/style.css" rel="stylesheet">
+    <link href="/public/bootstrap5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" />
-    <link rel="stylesheet" href="/app/public/css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <script src="/app/public/js/AlertElement.js"></script>
-    <script src="/app/public/js/js.js"></script>
+	<script src="/public/bootstrap5.3.0/js/bootstrap.min.js"></script>
+    <script src="/public/js/jquery-3.6.4.min.js"></script>
+    <script src="/public/js/classes/AlertElement.js"></script>
+    <script src="/public/js/js.js"></script>
 </head>
 
 <body class="bg-dark text-white h-100">
     <div id="blur" style="display: none;"></div>
-    <div class="alerts" id="alerts">
+    <div class="fixed-top m-3" id="alerts">
     </div>
     <div class="d-flex h-100 flex-column m-2">
         <nav class="navbar navbar-dark navbar-expand-sm px-3 rounded" style="background-color: #1f2429; box-shadow: 1px 5px 20px black; flex: 0 0 auto;">
@@ -29,9 +28,9 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link link-success" href="/" id="nav-home">Главная</a></li>
-                    <li class="nav-item"><a class="nav-link link-success" href="/courses" id="nav-courses">Курсы</a></li>
-                    <li class="nav-item"><a class="nav-link link-success" href="/users?min=0&max=10" id="nav-users">Пользователи</a></li>
-                    <li class="nav-item"><a class="nav-link link-success" href="/user?id=<?= $user->getId(); ?>" id="nav-profile">Мой профиль</a></li>
+                    <li class="nav-item"><a class="nav-link link-success" href="/courses?offset=0&limit=5" id="nav-courses">Курсы</a></li>
+                    <li class="nav-item"><a class="nav-link link-success" href="/users?offset=0&limit=5" id="nav-users">Пользователи</a></li>
+                    <li class="nav-item"><a class="nav-link link-success" href="/user?id=" id="nav-profile">Мой профиль</a></li>
                 </ul>
             </div>
         </nav>
