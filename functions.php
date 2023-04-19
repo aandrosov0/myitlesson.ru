99edb042d7bac9263deab32ff63ffe29f299674e
aandrosov <aandrosov338@gmail.com>
2023-04-19 21:55:00 +0300

    added authUser function

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php 
 namespace App;
 
+use App\Entity\UserEntity;
+use App\View\TokenView;
+
 function getOnlyClassName(string $className) {
 	$className = explode('\\', $className);
     return end($className);
+}
+
+function authUser() {
+    return match($_SERVER['REQUEST_METHOD']) {
+        'POST' => isset($_POST['token']) ? TokenView::decryptToken($_POST['token']) : null,
+        'GET' => isset($_SESSION['ID']) ? UserEntity::find($_SESSION['ID']) : null,
+    };
 }
