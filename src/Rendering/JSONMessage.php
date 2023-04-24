50b9338315f8f2dc76c50270bce25d3da28d7969
aandrosov <aandrosov338@gmail.com>
2023-04-24 16:11:21 +0300

    changed contructor

--- src/Rendering/JSONMessage.php
+++ src/Rendering/JSONMessage.php
@@ -1,8 +1,10 @@
 <?php 
 namespace App\Rendering;
 
+use stdClass;
+
 class JSONMessage extends Message {
-    public function __construct(array $message, int $responseCode = 200) {
+    public function __construct(array|stdClass $message = new stdClass, int $responseCode = 200) {
         parent::__construct(json_encode($message, JSON_UNESCAPED_UNICODE), $responseCode, 'Content-Type: application/json; charset=utf-8');
     }
 }
